fb7856d8f2b91f66db607836428ee09e42c9ad57
Will Parks <williamparks94@icloud.com>
2022-03-03 16:05:37 -0500

    Add Accounts Component

---
+++ src/components/accounts/accounts.js
@@ -0,0 +1,14 @@
+export const accounts = [
+    {
+        type: "Brokerage",
+        balance: 1000.00
+    },
+    {
+        type: "401K",
+        balance: 1000000.00
+    },
+    {
+        type: "Roth IRA",
+        balance: 6500.00
+    }
+];
--- src/components/accounts/index.js
+++ src/components/accounts/index.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
+import { TableBody, Table, TableCell, TableContainer, TableHead, TableRow, Paper} from "@material-ui/core"
+import { accounts } from "./accounts";
 
 export default class Accounts extends Component {
-    render() {
-        return (
-          <div>
-            <p>Accounts works!</p>
-          </div>  
-        );
-    }
+
+  rows = accounts;
+
+  render() {
+    return (
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Account Type</TableCell>
+              <TableCell>Balance</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.rows.map((row) => (
+              <TableRow key={row.type}>
+                <TableCell component="th" scope="row">
+                  {row.type}
+                </TableCell>
+                <TableCell>{row.balance}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
 }
