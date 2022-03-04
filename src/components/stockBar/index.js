69221552cbf5340515ba912e97bb69c4b985ed89
valfragier16 <valfragier@gmail.com>
2022-03-04 11:39:44 -0500

    fix grid

--- src/components/stockBar/index.js
+++ src/components/stockBar/index.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react'
-import Ticker from 'react-ticker'
+import React, { Component } from 'react';
 
-export default class StockBar extends Component {
+export default class TopBar extends Component {
     render() {
         return (
             <div className={this.props.className}>
-                <a href="/">
-                    <p><b>Markets</b> as of 3/3/2022 4:30 p.m., ET </p>
-                </a>
-                <a href="/" className="stocks">
-                <p>You have 0 unreas messages</p>
-                    <Ticker>
-                        {({ index }) => (
-                            <>
-                            <h1 style={{ paddingRight: "0.5em" }}>
-                                NASDAQ #{index}!
-                            </h1>
-                            </>
-                        )}
-                    </Ticker>
-               </a>
+                <div className="row">
+                    <div className="column-item">
+                        <p><b>Markets</b> as of 3/4/3033 11:30 a.m., et </p>
+                    </div>
+                    <div className="column-item">
+                        <p>DIJA 18,132.70</p>
+                    </div>
+
+                </div>
             </div>
         );
     }
--- src/components/top-bar/index.js
+++ src/components/top-bar/index.js
@@ -6,12 +6,21 @@ export default class TopBar extends Component {
             <div className={this.props.className}>
                 <a href="/">
                     <h1>Welcome back</h1>
-                    <p>Email:</p>
+                    <div className="row">
+                        <div className="column left">
+                            <p>Email:</p>
+                        </div>
+                        <div className="column middle">
+                            <p>Last Logon: {/*March 7, 2022 08:52 a.m., Eastern Time*/}</p>
+                        </div>
+                        <div className="column right">
+                            {/*<p>You have 0 unread messages</p>
+                            <a href="/" className="button fancy-button">
+                                <i className="material-icons">email</i>
+                            </a>*/}
+                        </div>
+                    </div>
                 </a>
-                <a href="/" className="button fancy-button">
-                You have 0 unread messages
-                    <i className="material-icons">email</i>
-               </a>
             </div>
         );
     }
